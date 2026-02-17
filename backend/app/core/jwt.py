9ed13297d7f95df5300bccdd7588652eaf495fa4
Realtor-express <sherzod909699899@gmail.com>
2026-02-17 13:11:30 +0500

    Add JWT token creation

---
+++ backend/app/core/jwt.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+from app.core.config import settings
+
+
+def create_access_token(user_id: str) -> str:
+    expire = datetime.utcnow() + timedelta(minutes=settings.JWT_ACCESS_EXPIRES_MIN)
+
+    payload = {
+        "sub": user_id,
+        "exp": expire,
+    }
+
+    return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.JWT_ALG)
